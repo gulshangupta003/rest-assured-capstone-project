1a83c885c2a90dd45754e1dd3b46574200ae90bc
gulshangupta1 <gulshang@testvagrant.com>
2022-10-10 12:07:14 +0530

    Dynamic id. Where index=1.

--- src/test/java/posts/postTests/DeletePostTests.java
+++ src/test/java/posts/postTests/DeletePostTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import posts.PostsService;
 import posts.deletePost.DeletePostResponse;
+import posts.getAllPosts.GetAllPostsResponse;
 
 import static org.testng.Assert.*;
 
@@ -12,11 +13,14 @@ public class DeletePostTests {
     // 1. Arrange
     private PostsService postsService;
     private String id;
+    GetAllPostsResponse getAllPostsResponse;
 
     @BeforeClass
     public void beforeClass() {
         postsService = new PostsService();
-        id = "634007774887a23b64c19a53";
+//        id = "634007774887a23b64c19a53";
+        getAllPostsResponse = new PostsService().getAllPosts();
+        id = getAllPostsResponse.getDataList().get(0).getId();
     }
 
     @Test
